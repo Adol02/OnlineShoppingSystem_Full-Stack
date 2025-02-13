ccd507e385470520e6aa65fb082011382ee672f8
Hareni <hareniraj2002@gmail.com>
2025-02-13 11:38:25 +0530

    Create UserInfo.java

---
+++ OnlineShoppingSystemSpringBoot/src/main/java/com/example/model/UserInfo.java
@@ -0,0 +1,61 @@
+package com.example.model;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserInfo implements UserDetails {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String username;
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public UserInfo(User user) {
+        this.username = user.getEmail(); // Assuming email is used as username
+        this.password = user.getPassword();
+        this.authorities = List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
